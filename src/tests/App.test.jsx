ab64bc5621de7ab6cb5ef2cd46b49701c3ff08f9
PhilipVigus <philipvigus@yahoo.co.uk>
2020-09-04 11:13:11 +0100

    Fix bug in App test

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,13 +1,50 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import App from "../App";
 
 describe("App", () => {
-  it("renders the App", () => {
+  let mock;
+
+  beforeAll(() => {
+    mock = new MockAdapter(axios);
+  });
+
+  afterEach(() => {
+    mock.reset();
+  });
+
+  afterAll(() => {
+    mock.restore();
+  });
+
+  it("renders the App", async () => {
+    mock
+      .onGet(
+        "https://vvgv5rubu3.execute-api.eu-west-2.amazonaws.com/dev/sessions"
+      )
+      .replyOnce(200, {
+        data: [
+          {
+            id: "1235",
+            status: "POSTED",
+            startDatetime: "2020-10-19T08:30:00+00:00",
+            endDatetime: "2020-10-19T16:15:00+00:00",
+            applicationIds: [123, 234],
+            practice: { id: "1235", name: "Manchester Hospital" },
+            locum: null,
+            hourlyRate: 85,
+            staffType: "gp",
+            staffTypeId: "1"
+          }
+        ]
+      });
+
     render(<App />);
 
     expect(
-      screen.getByRole("heading", { name: "Session List" })
+      await screen.findByRole("heading", { name: "Session List" })
     ).toBeInTheDocument();
   });
 });
